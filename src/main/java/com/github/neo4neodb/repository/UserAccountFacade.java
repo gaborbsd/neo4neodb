cb8f98b6624353a3122b4ba3d6d1535d7ec017f6
Gábor Kövesdán <gabor@kovesdan.org>
2013-10-16 15:15:48 +0200

    - Add email handling

--- src/main/java/com/github/neo4neodb/repository/UserAccountFacade.java
+++ src/main/java/com/github/neo4neodb/repository/UserAccountFacade.java
@@ -4,8 +4,11 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.text.MessageFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Component;
 
 import com.github.neo4neodb.domain.Observer;
@@ -13,6 +16,20 @@ import com.github.neo4neodb.domain.Observer;
 @Component
 public class UserAccountFacade {
 
+	private static final String REGISTRATION_MSG = "Dear {0},\n\nthank you for your "
+			+ "registration on Neo4NeoDB. To log in, first you have to activate "
+			+ "you registration.  Your activation code is:\n\n{1}\n\n\n"
+			+ "The Neo4NeoDB Team";
+	private static final String REGISTRATION_SUBJ = "Neo4NeoDB Registration";
+	private static final String PWRESET_MSG = "Dear {0},\n\nit seems someone "
+			+ "has initiated a password recovery for your account.  You can change "
+			+ "your password with the following activation code:\n\n{1}\n\n"
+			+ "It the change was not requested by you, please ignore this message.\n\n\n"
+			+ "The Neo4NeoDB Team";
+	private static final String PWRESET_SUBJ = "Neo4NeoDB Password Recovery";
+
+	@Autowired
+	private MailSender mailSender;
 	@Autowired
 	private ObserverRepository repo;
 	private static SecureRandom random = new SecureRandom();
@@ -39,7 +56,13 @@ public class UserAccountFacade {
 		String confirmationCode = genConfirmationCode();
 		Observer o = new Observer(name, hash(pass), email, confirmationCode);
 		repo.save(o);
-		// XXX : send email
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setFrom("noreply@neo4neodb.com");
+		msg.setTo(email);
+		msg.setSubject(REGISTRATION_SUBJ);
+		msg.setText(MessageFormat.format(REGISTRATION_MSG, name,
+				confirmationCode));
+		mailSender.send(msg);
 	}
 
 	public boolean activate(long uid, String confirmationCode) {
@@ -61,8 +84,14 @@ public class UserAccountFacade {
 					+ "the specified id.");
 		String confirmationCode = genConfirmationCode();
 		o.setConfirmationCode(confirmationCode);
-		// XXX: send mail
 		repo.save(o);
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setFrom("noreply@neo4neodb.com");
+		msg.setTo(o.getEmail());
+		msg.setSubject(PWRESET_SUBJ);
+		msg.setText(MessageFormat.format(PWRESET_MSG, o.getName(),
+				confirmationCode));
+		mailSender.send(msg);
 	}
 
 	public boolean resetPassword(long uid, String confirmationCode, String pass) {
