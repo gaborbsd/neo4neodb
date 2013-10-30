38ccb77718a4bfe3ac5b9ed4dedbcaa25b082961
Gábor Kövesdán <gabor@kovesdan.org>
2013-10-30 14:39:47 +0100

    - Work around Spring Data Neo4j limitation by adding a START clause

--- src/main/java/com/github/neo4neodb/repository/ObservationRepository.java
+++ src/main/java/com/github/neo4neodb/repository/ObservationRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.neo4j.repository.GraphRepository;
 import com.github.neo4neodb.domain.Observation;
 
 public interface ObservationRepository extends GraphRepository<Observation> {
-	@Query("MATCH (u:Observer)-[r:OBSERVED]->(o:Observation) "
+	@Query("START u=node(*) "
+			+ "MATCH (u:Observer)-[r:OBSERVED]->(o:Observation) "
 			+ "WHERE {0} * 0.95 < o.ra AND o.ra < {0} * 1.05 "
 			+ "AND {1} - 1 < o.dec AND o.dec < {1} + 1 "
 			+ "AND r.date > {2} - 172800000 " + "RETURN o, r.date "
@@ -14,13 +15,15 @@ public interface ObservationRepository extends GraphRepository<Observation> {
 	Iterable<Observation> getSimilarEntries(long rightAscension,
 			double declination, long date);
 
-	@Query("MATCH u:Observer, o:Observation "
+	@Query("START u=node(*) "
+			+ "MATCH u:Observer, o:Observation "
 			+ "WHERE u.id = {0} "
 			+ "AND o.id = {1} "
 			+ "CREATE (u-[:OBSERVED {date: {2}}]->o")
 	void confirmObservation(long uid, long oid, long date);
 
-	@Query("MATCH u:Observer "
+	@Query("START u=node(*) "
+			+ "MATCH u:Observer "
 			+ "WHERE u.id = {0} "
 			+ "CREATE u-[:OBSERVED {date: {2}}]->({1}))")
 	void createNewObservation(long id, Observation o, long date);
