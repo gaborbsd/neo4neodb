4b82e36fb12a937d122b4455b988631d32a05cfe
Gábor Kövesdán <gabor@kovesdan.org>
2013-12-18 16:30:56 +0100

    - Add some convenience methods so that this can be used as a   single interface

--- src/main/java/com/github/neo4neodb/repository/ObservationService.java
+++ src/main/java/com/github/neo4neodb/repository/ObservationService.java
@@ -1,5 +1,6 @@
 package com.github.neo4neodb.repository;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,18 @@ public class ObservationService {
 	@Autowired
 	private ObservationRepository repo;
 
+	public Iterable<Observation> findAll() {
+		return repo.findAll();
+	}
+
+	public Observation findOne(Long id) {
+		return repo.findOne(id);
+	}
+
+	public void delete(Long id) {
+		repo.delete(id);
+	}
+
 	public void addObserverForExistingObservation(Observation o, Observer u) {
 		Observation o1 = repo.findOne(o.getId());
 		o1.observedBy(u, new Date().getTime());
@@ -22,5 +35,10 @@ public class ObservationService {
 	public void newObservationForObserver(Observation o, Observer u) {
 		o.observedBy(u, new Date().getTime());
 		repo.save(o);
+	}
+
+	public Iterable<Observation> getSimilarObservations(Observation o) {
+		return repo.getSimilarEntries(o.getRightAscension(),
+				o.getDeclination(), Calendar.getInstance().getTimeInMillis());
 	}
 }
